42a09957edaa23052a1d945016fcecf9ea9d8ad1
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2024-02-11 21:55:45 +0700

    header finished

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,12 +7,48 @@ import "./global.css";
 export default function RootLatout({ children }) {
   return (
     <html lang='en' className={montserrat.variable}>
-      <body className='py-8 px-20 flex flex-col min-h-screen font-montserrat'>
-        <header>
+      <body className=' flex flex-col min-h-screen font-montserrat'>
+        <header className='bg-[#FFD4D0] pt-8 px-36'>
           <Header />
+
+          <div className='flex flex-row items-center pt-10 '>
+            <div className='flex flex-col gap-8'>
+              <div className='font-normal text-xl'>
+                <p>UX Designer</p>
+              </div>
+              <div>
+                <h1 className='font-bold text-5xl '>Hi There, I’m </h1>
+              </div>
+              <div className='text-[#FC3314] font-bold text-5xl'>
+                Sarah Jones
+              </div>
+              <div>
+                <p className='text-base font-normal '>
+                  Welcome to my portfolio of captivating digital experiences.
+                  Explore my work and let's create something extraordinary
+                  together.
+                </p>
+              </div>
+              <div className='flex flex-row gap-5 items-center'>
+                <div>
+                  <button className='py-2 px-10 bg-black text-white text-base font-bold'>
+                    Hire Me
+                  </button>
+                </div>
+                <div>
+                  <button className='py-2 px-10 bg-white border-[1px] border-black text-black text-base font-bold'>
+                    Portfolio
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div>
+              <img src='/images/main-image.png' alt='' />
+            </div>
+          </div>
         </header>
-        <main className='grow'>{children}</main>
-        <footer>
+        <main className='grow py-8 px-36'>{children}</main>
+        <footer className='py-8 px-36'>
           <Footer />
         </footer>
       </body>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,39 @@
 /** @format */
 
 export default function Header() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <div className='relative flex flex-row justify-between font-bold text-lg items-center '>
+        <div className='absolute -top-10 left-[350px] '>
+          <img src='/images/half-circle.png' alt='' />
+        </div>
+        <div className='z-10'>
+          <img src='/images/main-logo.png' alt='' />
+        </div>
+        <nav className='z-10'>
+          <ul className='flex flex-row gap-8 '>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Services</a>
+            </li>
+            <li>
+              <a>Portfolio</a>
+            </li>
+            <li>
+              <a>Blog</a>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <a
+            href=''
+            className='py-2 px-4 bg-black text-white text-base font-bold'>
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
